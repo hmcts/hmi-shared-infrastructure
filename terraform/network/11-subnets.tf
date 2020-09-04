7c5ffb08a94687008fa04c57788b20a898e72678
karanotts <nottinghamkara@gmail.com>
2020-09-04 09:20:48 +0100

    remove gw subnet

--- terraform/network/11-subnets.tf
+++ terraform/network/11-subnets.tf
@@ -2,7 +2,7 @@ resource "azurerm_subnet" "apim_subnet" {
   name                      = "apim-subnet-${var.environment}"
   resource_group_name       = var.resource_group
   virtual_network_name      = azurerm_virtual_network.vnet.name
-  address_prefixes          = element(var.subnet_address_prefixes, 0)
+  address_prefixes          = var.subnet_address_prefixes
 
   dynamic "delegation" {
     for_each = var.subnet_delegation == null ? [] : ["delegation"]
@@ -16,11 +16,4 @@ resource "azurerm_subnet" "apim_subnet" {
   }
   enforce_private_link_endpoint_network_policies = true
   service_endpoints                              = var.service_endpoints
-}
-
-resource "azurerm_subnet" "perf_test_subnet" {
-  name                      = "gw-subnet-${var.environment}"
-  resource_group_name       = var.resource_group
-  virtual_network_name      = azurerm_virtual_network.vnet.name
-  address_prefixes          = element(var.subnet_address_prefixes, 1)
 }
