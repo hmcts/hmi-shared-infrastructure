4164203be43f77d5fde1a68de052f869821b441d
karanotts <nottinghamkara@gmail.com>
2020-09-28 19:17:00 +0100

    rename subnet

--- terraform/network/11-subnets.tf
+++ terraform/network/11-subnets.tf
@@ -7,7 +7,7 @@ resource "azurerm_subnet" "apim_subnet" {
 }
 
 resource "azurerm_subnet" "mgmt_subnet" {
-  name                      = "pact-subnet-${var.environment}"
+  name                      = "mgmt-subnet-${var.environment}"
   resource_group_name       = var.resource_group
   virtual_network_name      = azurerm_virtual_network.vnet.name
   address_prefixes          = [element(var.subnet_address_prefixes, 1)]
