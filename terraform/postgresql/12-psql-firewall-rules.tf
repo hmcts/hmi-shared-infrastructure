b24ec6f4a1644ae19c0fcba04b3bdf18fb570d38
karanotts <nottinghamkara@gmail.com>
2020-09-29 10:36:11 +0100

    added allow_all_azure fw rule

--- terraform/postgresql/12-psql-firewall-rules.tf
+++ terraform/postgresql/12-psql-firewall-rules.tf
@@ -1,5 +1,5 @@
 resource "azurerm_postgresql_firewall_rule" "allow_all_azure" {
-  name                = "Allow access to Azure services"
+  name                = "azure"
   resource_group_name = var.resource_group
   server_name         = azurerm_postgresql_server.hmi_pact.name
   start_ip_address    = "0.0.0.0"
