32f729ba1d54b7b3c045ff3481404c6e498f77d3
karanotts <nottinghamkara@gmail.com>
2020-10-02 14:16:45 +0100

    tf test

---
+++ pipeline/scripts/terraform-lint.tests.ps1
@@ -0,0 +1,31 @@
+$RootPath = Split-Path -Parent $MyInvocation.MyCommand.Path | Split-Path
+$TfFiles = @(@((Get-ChildItem $RootPath -filter '*.tf' -Recurse)))
+
+if ($TfFiles.Count -eq 0)
+{
+  Write-Output 'No Terraform files found in this folder'
+}
+else 
+{
+  Describe 'Terraform files validation' {
+    $TfTestCases = @()
+    $TfFiles.ForEach{$TfTestCases += @{Instance = $_}}
+
+
+    Context "Check for non zero length files" {
+      It "<Instance> file is greater than 0" -TestCases $TfTestCases {
+        Param($Instance)
+        (Get-ChildItem $Instance -Verbose).Length | Should BeGreaterThan 0
+      }  
+    }
+
+    $TfFolderTestCases=@()
+    ((($TfFiles).DirectoryName | Select-Object -Unique)).ForEach{$TfFolderTestCases += @{Instance = $_}}
+      Context "Are correctly formatted" {
+        It "All files in <Instance> are correctly formatted" -TestCases $TfFolderTestCases {
+          Param($Instance)
+          Invoke-Expression "terraform fmt -check=true $Instance"  | should BeNullOrEmpty            
+      }
+    }
+  }
+}
