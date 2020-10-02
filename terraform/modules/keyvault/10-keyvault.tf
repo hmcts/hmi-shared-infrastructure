4a2e0ad2d0e49471f6befc992e08e8e80dbe9252
karanotts <nottinghamkara@gmail.com>
2020-10-02 09:39:03 +0100

    Revert "update database"
    This reverts commit 395778c86de11deb210ca92933dd30c23f9a1eb9.

--- terraform/modules/keyvault/10-keyvault.tf
+++ terraform/modules/keyvault/10-keyvault.tf
@@ -1,9 +1,4 @@
-locals {
-  database_name = "hmi-sharedservices-kv-${var.environment}"
-  database_rg   = "hmi-sharedservices-${var.environment}-rg"
-}
-
 data "azurerm_key_vault" "shared_kv" {
-  name                = local.database_name.value
-  resource_group_name = local.database_rg.value
+  name                = "hmi-sharedservices-kv-${var.environment}"
+  resource_group_name = "hmi-sharedservices-${var.environment}-rg"
 }
