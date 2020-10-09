fa394ec8ee27597ed27580cdab97f3cf67659bd3
karanotts <nottinghamkara@gmail.com>
2020-10-09 08:40:23 +0100

    add ping test

---
+++ terraform/modules/network/01-inputs-required.tf
@@ -0,0 +1,10 @@
+variable "environment" {}
+variable "resource_group" {}
+variable "product" {}
+variable "location" {}
+variable "address_space" {}
+variable "tags" {}
+variable "subnet_address_prefixes" {}
+variable "apim_nsg_rules" {}
+variable "apim_rules" {}
+variable "route_table" {}
