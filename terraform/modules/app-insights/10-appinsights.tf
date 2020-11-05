cc1f2663aa8e95a0f78b163688c50a8e549ff957
karanotts <nottinghamkara@gmail.com>
2020-11-05 13:12:37 +0000

    appins > other

--- terraform/modules/app-insights/10-appinsights.tf
+++ terraform/modules/app-insights/10-appinsights.tf
@@ -3,6 +3,6 @@ resource "azurerm_application_insights" "app_insights" {
   location            = var.location
   resource_group_name = var.resource_group
   tags                = var.tags
-  application_type    = "web"
+  application_type    = "other"
   retention_in_days   = var.environment == "sbox" || var.environment == "dev" || var.environment == "test" ? 30 : 60
 }
