75eb7cf550232471881ae9d971239fbe3504bd8d
karanotts <nottinghamkara@gmail.com>
2020-12-11 09:28:21 +0000

    test provider

--- terraform/modules/network/14-network-watcher.tf
+++ terraform/modules/network/14-network-watcher.tf
@@ -4,7 +4,7 @@ data "azurerm_network_watcher" "network_watcher" {
 }
 
 data "azurerm_log_analytics_workspace" "hmcts" {
-  provider            = "log-analytics-subscription"
+  provider            = azurerm.log-analytics-subscription
   name                = var.log_analytics_workspace_name
   resource_group_name = var.log_analytics_workspace_rg
 }
