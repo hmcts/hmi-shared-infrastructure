a45d1249f34bf75fb31b0bdae9e40a08cf06d25f
karanotts <nottinghamkara@gmail.com>
2020-12-11 10:52:30 +0000

    correct netwatcher rg

--- terraform/modules/network/14-network-watcher.tf
+++ terraform/modules/network/14-network-watcher.tf
@@ -1,14 +1,3 @@
-data "azurerm_network_watcher" "network_watcher" {
-  name                = "NetworkWatcher_${var.location}"
-  resource_group_name = "NetworkWatcherRG"
-}
-
-data "azurerm_log_analytics_workspace" "hmcts" {
-  provider            = azurerm.log-analytics-subscription
-  name                = var.log_analytics_workspace_name
-  resource_group_name = var.log_analytics_workspace_rg
-}
-
 resource "azurerm_storage_account" "network_watcher_storage" {
   name                = "hmiapimnetworkwatcher${var.location}"
   resource_group_name = var.resource_group
@@ -23,7 +12,7 @@ resource "azurerm_storage_account" "network_watcher_storage" {
 
 resource "azurerm_network_watcher_flow_log" "network_watcher_flow" {
   network_watcher_name = data.azurerm_network_watcher.network_watcher.name
-  resource_group_name  = data.azurerm_resource_group.network_watcher.name
+  resource_group_name  = data.azurerm_resource_group.network_watcher.resource_group_name
 
   network_security_group_id = azurerm_network_security_group.apim_nsg.id
   storage_account_id        = azurerm_storage_account.network_watcher_storage.id
