5653cc4f010d2c1a6cc6aae48587b97c22eba8ee
karanotts <nottinghamkara@gmail.com>
2020-12-11 11:33:24 +0000

    correct data source

--- terraform/modules/postgresql/05-data-sources.tf
+++ terraform/modules/postgresql/05-data-sources.tf
@@ -7,14 +7,3 @@ data "azurerm_key_vault_secret" "pact_password" {
   name         = "pact-db-password"
   key_vault_id = data.azurerm_key_vault.shared_kv.id
 }
-
-data "azurerm_network_watcher" "network_watcher" {
-  name                = "NetworkWatcher_${var.location}"
-  resource_group_name = "NetworkWatcherRG"
-}
-
-data "azurerm_log_analytics_workspace" "hmcts" {
-  provider            = azurerm.log-analytics-subscription
-  name                = var.log_analytics_workspace_name
-  resource_group_name = var.log_analytics_workspace_rg
-}
