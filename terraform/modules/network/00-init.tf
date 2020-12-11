b84f0132750519bf8fd307982be83aa276587983
karanotts <nottinghamkara@gmail.com>
2020-12-11 09:24:18 +0000

    test provider

--- terraform/modules/network/00-init.tf
+++ terraform/modules/network/00-init.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.12.0"
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  version = ">=2.0.0"
+  features {}
+}
+
 provider "azurerm" {
   subscription_id = var.log_analytics_subscription_id
   alias           = "log-analytics-subscription"
