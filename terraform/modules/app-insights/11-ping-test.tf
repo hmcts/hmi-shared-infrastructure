9487ae16041ee48c096cdaa38fc2ae6fc718107a
karanotts <nottinghamkara@gmail.com>
2021-02-01 06:59:12 +0000

    add web alert

--- terraform/modules/app-insights/11-ping-test.tf
+++ terraform/modules/app-insights/11-ping-test.tf
@@ -12,3 +12,30 @@ resource "azurerm_application_insights_web_test" "ping_test" {
   configuration           = templatefile("../../modules/app-insights/ping-test.tmpl", { url = lookup(var.ping_tests[count.index], "health_check_url") })
   tags                    = var.tags
 }
+
+resource "azurerm_monitor_metric_alert" "web_alert" {
+  name                = "example-metricalert"
+  count               = length(var.ping_tests)
+  resource_group_name = azurerm_application_insights_web_test.ping_test.resource_group_name
+  scopes              = "${element(azurerm_application_insights_web_test.ping_test.*.id, count.index)}"
+  description         = "Hello World"
+
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "Http5xx"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 5
+
+    dimension {
+      name     = "Instance"
+      operator = "Include"
+      values   = ["*"]
+    }
+  }
+
+  frequency   = "PT1M"
+  window_size = "PT5M"
+  severity    = 3
+
+}
