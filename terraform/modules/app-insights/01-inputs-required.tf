232b905ffbc6cbe8c0feb48699d61d9a10096cd9
karanotts <nottinghamkara@gmail.com>
2021-02-02 11:39:25 +0000

    corrected email var

--- terraform/modules/app-insights/01-inputs-required.tf
+++ terraform/modules/app-insights/01-inputs-required.tf
@@ -3,6 +3,6 @@ variable "resource_group" {}
 variable "product" {}
 variable "location" {}
 variable "tags" {}
-variable "support_email    = var.email_support" {
+variable "support_email" {
   default = "kara.nottingham1@hmcts.net"
 }
