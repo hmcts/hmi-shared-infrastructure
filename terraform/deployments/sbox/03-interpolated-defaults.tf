6e7f6332612d47929605788f3f9868746009ce24
Felix Eyetan <peyetan@yahoo.com>
2021-06-14 16:02:46 +0100

    Consolidating duplicate files

--- terraform/deployments/sbox/03-interpolated-defaults.tf
+++ terraform/deployments/sbox/03-interpolated-defaults.tf
@@ -15,3 +15,15 @@ data "azurerm_virtual_network" "hmi" {
   name                = "hmi-sharedinfra-vnet-${var.environment}"
   resource_group_name = data.azurerm_resource_group.hmi.name
 }
+
+# Generic locals
+locals {
+  common_tags = module.ctags.common_tags
+}
+
+module "ctags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.environment
+  product     = var.product
+  builtFrom   = var.builtFrom
+}
