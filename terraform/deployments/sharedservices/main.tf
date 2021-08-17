fe2fff35997b52b399fabf0045f19f1937348345
Christopher Pateman <christopher.pateman@version1.com>
2021-08-17 09:34:56 +0100

    HMIS-913 Add TF access for KV

--- terraform/deployments/sharedservices/main.tf
+++ terraform/deployments/sharedservices/main.tf
@@ -53,6 +53,14 @@ module "keyvault-policy" {
   key_vault_id = module.kv.key_vault_id
 
   policies = {
+    "tf-access" = { # This Access is for Terraform to correctly create and destroy
+      tenant_id               = data.azurerm_client_config.current.tenant_id
+      object_id               = data.azurerm_client_config.current.object_id
+      key_permissions         = ["backup", "create", "decrypt", "delete", "encrypt", "get", "import", "list", "purge", "recover", "restore", "sign", "unwrapKey", "update", "verify", "wrapKey"]
+      secret_permissions      = ["backup", "delete", "get", "list", "purge", "recover", "restore", "set"]
+      certificate_permissions = ["create", "delete", "deleteissuers", "get", "getissuers", "import", "list", "listissuers","managecontacts", "manageissuers", "purge", "recover", "setissuers", "update", "backup", "restore"]
+      storage_permissions     = ["backup", "delete", "deletesas", "get", "getsas", "list", "listsas","purge", "recover", "regeneratekey", "restore", "set", "setsas", "update"]
+    },
     "cft-client" = {
       tenant_id               = data.azurerm_client_config.current.tenant_id
       object_id               = "e7214bce-c8df-4d67-b081-1def53db25a7" #TODO: get id from data source
