18d3f124b6fbd32f445ef01beeef9ccba85ffb74
Christopher Pateman <christopher.pateman@version1.com>
2021-08-26 14:43:45 +0100

    Correct product name

--- terraform/deployments/sharedservices/main.tf
+++ terraform/deployments/sharedservices/main.tf
@@ -11,7 +11,7 @@ locals {
 module "ctags" {
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
   environment = var.environment
-  product     = var.product
+  product     = "hmi"
   builtFrom   = var.builtFrom
 }
 
