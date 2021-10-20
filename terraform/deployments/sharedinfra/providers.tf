4b42fa51b01950dd76450935f82c0e36f4dc8dc0
Christopher Pateman <christopher.pateman@version1.com>
2021-10-20 16:11:38 +0100

    HMIS-944 update tf versions

--- terraform/deployments/sharedinfra/providers.tf
+++ terraform/deployments/sharedinfra/providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {}
-  required_version = ">= 0.13.3"
+  required_version = ">= 0.14.0"
   required_providers {
     azurerm = {
       version = ">=2.0.0"
--- terraform/modules/network/00-init.tf
+++ terraform/modules/network/00-init.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.13.3"
+  required_version = ">= 0.14.0"
   required_providers {
     azurerm = {
       version = ">=2.0.0"
