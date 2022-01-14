b2023ea9179592e8546f865bad849793aff54984
Christopher Pateman <christopher.pateman@version1.com>
2022-01-14 15:52:58 +0000

    HMIS-984 Connect Vnet to Private DNS Zone

--- terraform/modules/network/00-init.tf
+++ terraform/modules/network/00-init.tf
@@ -12,3 +12,9 @@ provider "azurerm" {
   alias           = "log-analytics-subscription"
   features {}
 }
+provider "azurerm" {
+  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+  alias           = "private-dns-zone-subscription"
+  features {}
+}
+
--- terraform/modules/network/10-vnet.tf
+++ terraform/modules/network/10-vnet.tf
@@ -5,3 +5,15 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = var.resource_group
   tags                = var.tags
 }
+
+locals {
+  dns_zone_name = var.environment == "prod" ? "platform.hmcts.net" : "staging.platform.hmcts.net"
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+  provider              = azurerm.private-dns-zone-subscription
+  name                  = "${var.project}-vnet-${var.environment}"
+  resource_group_name   = "core-infra-intsvc-rg"
+  private_dns_zone_name = local.dns_zone_name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
