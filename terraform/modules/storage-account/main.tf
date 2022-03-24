97129b28b5b7748d2c1ab0eca836ec2280e88c69
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-03-24 15:03:02 +0000

    sa hmiapiminfrasboxsa

--- terraform/modules/storage-account/main.tf
+++ terraform/modules/storage-account/main.tf
@@ -19,3 +19,24 @@ module "sa" {
   team_name    = "HMI DevOps"
   team_contact = "#vh-devops"
 }
+
+module "sa_apim" {
+  source = "git::https://github.com/hmcts/cnp-module-storage-account.git?ref=master"
+  #source = "../../../cnp-module-storage-account"
+
+  env = var.env
+
+  storage_account_name = "hmiapiminfra${var.env}sa" 
+  common_tags          = var.common_tags
+
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  account_tier             = "Standard" # var.sa_account_tier
+  account_kind             = "StorageV2" # var.sa_account_kind
+  account_replication_type = "RAGRS" # var.sa_account_replication_type
+  access_tier              = "Hot" # var.sa_access_tier
+
+  team_name    = "HMI DevOps"
+  team_contact = "#vh-devops"
+}
