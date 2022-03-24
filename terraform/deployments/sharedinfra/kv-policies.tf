a163aaabd3502a31799464b7d597e8b4517ee995
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-03-24 08:28:26 +0000

    policy update

--- terraform/deployments/sharedinfra/kv-policies.tf
+++ terraform/deployments/sharedinfra/kv-policies.tf
@@ -23,14 +23,14 @@ module "keyvault-policy" {
   key_vault_id = module.kv.key_vault_id
 
   policies = {
-    "cft-client" = {
-      tenant_id               = data.azurerm_client_config.current.tenant_id
-      object_id               = data.azuread_application.cft_client.object_id
-      key_permissions         = []
-      secret_permissions      = ["get"]
-      certificate_permissions = []
-      storage_permissions     = []
-    },
+    # "cft-client" = {
+    #   tenant_id               = data.azurerm_client_config.current.tenant_id
+    #   object_id               = data.azuread_application.cft_client.object_id
+    #   key_permissions         = []
+    #   secret_permissions      = ["get"]
+    #   certificate_permissions = []
+    #   storage_permissions     = []
+    # },
     "aks-${var.environment}-mi" = {
       tenant_id               = data.azurerm_client_config.current.tenant_id
       object_id               = module.aks-mi.principal_id
