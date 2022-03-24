baae435263ef98fb7ffb573871fcafb829b18258
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-03-24 09:14:27 +0000

    delete bash kv policy4

--- terraform/deployments/sharedinfra/kv-policies.tf
+++ terraform/deployments/sharedinfra/kv-policies.tf
@@ -23,38 +23,37 @@ module "keyvault-policy" {
   key_vault_id = module.kv.key_vault_id
 
   policies = {
-    # "cft-client" = {
-    #   tenant_id               = data.azurerm_client_config.current.tenant_id
-    #   object_id               = data.azuread_application.cft_client.object_id
-    #   key_permissions         = []
-    #   secret_permissions      = ["get"]
-    #   certificate_permissions = []
-    #   storage_permissions     = []
-    # },
-    # "aks-${var.environment}-mi" = {
-    #   tenant_id               = data.azurerm_client_config.current.tenant_id
-    #   object_id               = module.aks-mi.principal_id
-    #   key_permissions         = []
-    #   secret_permissions      = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore"]
-    #   certificate_permissions = []
-    #   storage_permissions     = []
-    # },
-    # "${local.apimName}" = {
-    #   tenant_id               = data.azurerm_client_config.current.tenant_id
-    #   object_id               = data.azurerm_api_management.hmi_apim_svc.identity.0.principal_id
-    #   key_permissions         = []
-    #   secret_permissions      = ["Get", "Set", "List", "Delete"]
-    #   certificate_permissions = []
-    #   storage_permissions     = []
-    # },
-    "sp-${var.environment}" = {
+    "cft-client" = {
       tenant_id               = data.azurerm_client_config.current.tenant_id
-      object_id               = data.azurerm_client_config.current.object_id
+      object_id               = data.azuread_application.cft_client.object_id
       key_permissions         = []
-      secret_permissions      = ["Backup", "Get", "Set", "List", "Delete", "Purge", "Recover", "Restore"]
-      certificate_permissions = ["Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "SetIssuers", "Update"]
+      secret_permissions      = ["get"]
+      certificate_permissions = []
       storage_permissions     = []
-
-    }
+    },
+    "aks-${var.environment}-mi" = {
+      tenant_id               = data.azurerm_client_config.current.tenant_id
+      object_id               = module.aks-mi.principal_id
+      key_permissions         = []
+      secret_permissions      = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore"]
+      certificate_permissions = []
+      storage_permissions     = []
+    },
+    "${local.apimName}" = {
+      tenant_id               = data.azurerm_client_config.current.tenant_id
+      object_id               = data.azurerm_api_management.hmi_apim_svc.identity.0.principal_id
+      key_permissions         = []
+      secret_permissions      = ["Get", "Set", "List", "Delete"]
+      certificate_permissions = []
+      storage_permissions     = []
+    },
+    # "sp-${var.environment}" = {
+    #   tenant_id               = data.azurerm_client_config.current.tenant_id
+    #   object_id               = data.azurerm_client_config.current.object_id
+    #   key_permissions         = []
+    #   secret_permissions      = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
+    #   certificate_permissions = ["Backup", "Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "SetIssuers", "Update"]
+    #   storage_permissions     = []
+    # }
   }
 }
