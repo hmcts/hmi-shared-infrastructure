7d38de8e60ae2b352242fb06930ca8301171fafd
Adam Todd <adam.todd@hmcts.net>
2022-07-22 16:38:20 +0100

    changes

--- terraform/modules/logic-app/main.tf
+++ terraform/modules/logic-app/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_logic_app_workflow" "hmi_la_dturota" {
   name                = "hmi-la-dturota-${var.env}"
   location            = var.location
   resource_group_name = var.resource_group
-  
+
   tags = var.common_tags
 }
 
@@ -26,56 +26,55 @@ data "azurerm_subscription" "api_sub" {
 
 }
 
-data "azurerm_storage_account" "dtusa" {
-	name = "hmidtu${var.env}"
-	resource_group_name = var.resource_group
+data "azurerm_client_config" "current" {
+
 }
 
 module "hmidtu" {
   source = "../../modules/storage-account/data"
 
-  storage_account_name = "hmidtu${var.env}"
+  storage_account_name = var.dtu_sa
   resource_group_name  = var.resource_group
 }
 
 resource "azurerm_api_connection" "azureblob" {
-	name = "azureblob"
-	resource_group_name = var.resource_group
-	managed_api_id = "subscriptions/${data.azurerm_subscription.api_sub.subscription_id}/providers/Microsoft.Web/locations/${var.location}/managedApis/azureblob"
-	display_name = "HMI DTU Rota"
-	parameter_values = {
-		accountName = "hmidtu${var.env}"
-		accessKey = module.hmidtu.primary_access_key
-	}
+  name                = "azureblob"
+  resource_group_name = var.resource_group
+  managed_api_id      = "subscriptions/${data.azurerm_subscription.api_sub.subscription_id}/providers/Microsoft.Web/locations/${var.location}/managedApis/azureblob"
+  display_name        = "HMI DTU Rota"
+  parameter_values = {
+    accountName = var.dtu_sa
+    accessKey   = module.hmidtu.primary_access_key
+  }
 
-	tags = var.common_tags
+  tags = var.common_tags
 }
 
 resource "azurerm_api_connection" "keyvault" {
-	name = "keyvault"
-	resource_group_name = var.resource_group
-	managed_api_id =  "subscriptions/${data.azurerm_subscription.api_sub.subscription_id}/providers/Microsoft.Web/locations/${var.location}/managedApis/keyvault"
-	display_name = "HMI Key Vault"
-	parameter_values = {
-		"token:clientId" = "#{keyVaultUserId}#",
-		"token:clientSecret" = "#{keyVaultUserSecret}#",
-		"token:TenantId" = "#{azureTenant}#",
-		"token:grantType" = "client_credentials",
-		"vaultName" = "hmi-shared-kv-${var.env}"
-	}
+  name                = "keyvault"
+  resource_group_name = var.resource_group
+  managed_api_id      = "subscriptions/${data.azurerm_subscription.api_sub.subscription_id}/providers/Microsoft.Web/locations/${var.location}/managedApis/keyvault"
+  display_name        = "HMI Key Vault"
+  parameter_values = {
+    "token:clientId"     = "#{keyVaultUserId}#",
+    "token:clientSecret" = "#{keyVaultUserSecret}#",
+    "token:TenantId"     = data.azurerm_client_config.current.tenant_id,
+    "token:grantType"    = "client_credentials",
+    "vaultName"          = "hmi-shared-kv-${var.env}"
+  }
 
-	tags = var.common_tags
+  tags = var.common_tags
 }
 
 resource "azurerm_api_connection" "azuretables" {
-	name = "azuretables"
-	resource_group_name = var.resource_group
-	managed_api_id = "subscriptions/${data.azurerm_subscription.api_sub.subscription_id}/providers/Microsoft.Web/locations/${var.location}/managedApis/azuretables"
-	display_name = "HMI DTU Rota"
-	parameter_values = {
-	  storageaccount = "hmidtu${var.env}"
-	  sharedKey = data.azurerm_storage_account.dtusa.primary_access_key
-	}
+  name                = "azuretables"
+  resource_group_name = var.resource_group
+  managed_api_id      = "subscriptions/${data.azurerm_subscription.api_sub.subscription_id}/providers/Microsoft.Web/locations/${var.location}/managedApis/azuretables"
+  display_name        = "HMI DTU Rota"
+  parameter_values = {
+    storageaccount = var.dtu_sa
+    sharedKey      = data.azurerm_storage_account.dtusa.primary_access_key
+  }
 
-	tags = var.common_tags
+  tags = var.common_tags
 }
--- terraform/modules/logic-app/variables.tf
+++ terraform/modules/logic-app/variables.tf
@@ -1,13 +1,20 @@
 variable "env" {
   type = string
+  description = "The short name of the current enviornment"
 }
 variable "resource_group" {
   type = string
+  description = "Name of the resource group for the logic app"
 }
 variable "location" {
   type = string
+  description = "Geographical location of resource e.g uksouth"
 }
 variable "common_tags" {
   type        = map(string)
   description = "Tags for the Azure resources"
+}
+variable "dtu_sa" {
+	type = string
+	description = "Name for the Storage account"
 }
