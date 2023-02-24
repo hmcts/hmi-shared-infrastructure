2595956fa2e3351d6aec511f6ab8e0de7ae5a43e
Adam Todd <adam.todd@hmcts.net>
2023-02-24 14:58:03 +0000

    kv secrets

--- terraform/deployments/sharedinfra/kv-secrets.tf
+++ terraform/deployments/sharedinfra/kv-secrets.tf
@@ -7,77 +7,77 @@
 #  resource_group_name  = data.azurerm_resource_group.hmi.name
 #}
 #
-#module "keyvault_secrets" {
-#  source = "../../modules/key-vault/secret"
-#
-#  key_vault_id = module.kv.key_vault_id
-#  tags         = local.common_tags
-#  secrets = [
-#    {
-#      name         = "appins-resource-id"
-#      value        = module.app-insights.id
-#      tags         = {}
-#      content_type = ""
-#    },
-#    {
-#      name         = "appins-instrumentation-key"
-#      value        = module.app-insights.instrumentation_key
-#      tags         = {}
-#      content_type = ""
-#    },
-#    {
-#      name         = "hmisharedinfrasa-storageaccount-key"
-#      value        = module.storage.primary_access_key
-#      tags         = {}
-#      content_type = ""
-#    },
-#    {
-#      name         = "hmisharedinfrasa-storageaccount-name"
-#      value        = "hmisharedinfrasa"
-#      tags         = {}
-#      content_type = ""
-#    },
+module "keyvault_secrets" {
+  source = "../../modules/key-vault/secret"
+
+  key_vault_id = module.kv.key_vault_id
+  tags         = local.common_tags
+  secrets = [
+    {
+      name         = "appins-resource-id"
+      value        = module.app-insights.id
+      tags         = {}
+      content_type = ""
+    },
+    {
+      name         = "appins-instrumentation-key"
+      value        = module.app-insights.instrumentation_key
+      tags         = {}
+      content_type = ""
+    },
+    {
+      name         = "hmisharedinfrasa-storageaccount-key"
+      value        = module.storage.primary_access_key
+      tags         = {}
+      content_type = ""
+    },
+    {
+      name         = "hmisharedinfrasa-storageaccount-name"
+      value        = "hmisharedinfrasa"
+      tags         = {}
+      content_type = ""
+    },
 #    {
 #      name         = "dtu-storage-account-key"
 #      value        = module.hmidtu.primary_access_key
 #      tags         = {}
 #      content_type = ""
 #    },
-#    {
-#      name         = "hmi-servicenow-client"
-#      value        = var.service_now_client
-#      tags         = {}
-#      content_type = ""
-#    },
-#    {
-#      name         = "hmi-servicenow-secret"
-#      value        = var.service_now_secret
-#      tags         = {}
-#      content_type = ""
-#    }
-#  ]
-#
-#  depends_on = [
-#    module.keyvault-policy,
-#  ]
-#}
-#
-#module "keyvault_ado_secrets" {
-#  source = "../../modules/key-vault/secret"
-#
-#  key_vault_id = module.kv.key_vault_id
-#  tags         = local.common_tags
-#  secrets = [
-#    for secret in var.secrets_arr : {
-#      name  = secret.name
-#      value = secret.value
-#      tags = {
-#        "source" : "ado library"
-#      }
-#      content_type = ""
-#    }
-#  ]
-#  depends_on = [
-#    module.keyvault-policy,
-#  ]
-#}
+    {
+      name         = "hmi-servicenow-client"
+      value        = var.service_now_client
+      tags         = {}
+      content_type = ""
+    },
+    {
+      name         = "hmi-servicenow-secret"
+      value        = var.service_now_secret
+      tags         = {}
+      content_type = ""
+    }
+  ]
+
+  depends_on = [
+    module.keyvault-policy,
+  ]
+}
+
+module "keyvault_ado_secrets" {
+  source = "../../modules/key-vault/secret"
+
+  key_vault_id = module.kv.key_vault_id
+  tags         = local.common_tags
+  secrets = [
+    for secret in var.secrets_arr : {
+      name  = secret.name
+      value = secret.value
+      tags = {
+        "source" : "ado library"
+      }
+      content_type = ""
+    }
+  ]
+  depends_on = [
+    module.keyvault-policy,
+  ]
+}
